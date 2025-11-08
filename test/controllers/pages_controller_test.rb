1621092f47e267942542754bf38dd88a57e60592
asamonte41 <asamonte41@academic.rrc.ca>
2025-11-08 17:08:16 -0600

    editing test files

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,10 +3,11 @@ require "test_helper"
 class PagesControllerTest < ActionDispatch::IntegrationTest
   # Test the About page
   test "should get about" do
-    get about_url  # use the correct route helper
-    assert_response :success
-    assert_select "h1", "About Hey Arnold!"  # checks that your <h1> text exists
-  end
+  get about_url
+  assert_response :success
+  assert_select "h1", "This is for my Intro to Rails Project"
+end
+
 
   # Example: you can add more page tests if you create more pages
   # test "should get home" do
