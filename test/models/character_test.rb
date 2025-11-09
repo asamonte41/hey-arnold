3b1642229b2cbb9fbecede6484d94058e90fa5ca
asamonte41 <asamonte41@academic.rrc.ca>
2025-11-09 16:08:21 -0600

    fixing test actions

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CharacterTest < ActiveSupport::TestCase
   test "character is valid with required fields" do
-  character = characters(:one)
+  character = characters(:arnold)
   puts character.errors.full_messages
   assert character.valid?
 end
